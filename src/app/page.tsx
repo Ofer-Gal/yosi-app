13efe1bef2d54b0ce1ff9e807a4d42c820276148
Ofer Gal <ofergal@gmail.com>
2025-04-28 14:41:24 +0300

    center and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      {/* <div style={{ fontSize: '24px', textAlign: 'center',flex:'1 1 auto' }} > */}
+      <h1 className="text-center text-4xl font-bold" >{'יוסי גיגי'}</h1>
+      {/* </div> */}
+      <main className="flex flex-col gap-[16px] row-start-2 items-center sm:items-start">
         <Image
           title="ליצן"
           className="rounded-full"
@@ -13,6 +16,7 @@ export default function Home() {
           height={580}
           priority
         />
+
 
       </main>
       {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
